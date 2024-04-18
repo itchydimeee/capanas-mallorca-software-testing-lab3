8abcbd9969a730fec1c0bb8066c4b94a8bbae958
itchydimeee <124387580+itchydimeee@users.noreply.github.com>
2024-04-18 23:07:50 +0800

    auth50percentpower

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,15 +5,15 @@ import PogsList from './screens/readPogs';
 import NewPage from './screens/newPage';
 import RenderPogs from './screens/renderPogs';
 import LoginForm from './screens/login';
-import SignupForm from './screens/signup';
 import UserPage from './screens/user';
+import AdminLogin from './screens/adminLogin';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginForm/>} />
-        <Route path="/signup" element={<SignupForm/>} />
+        <Route path="/adminLogin" element={<AdminLogin/>} />
         <Route path="/pogs" element={<PogsForm />} />
         <Route path="/readPogs" element={<PogsList />} />
         <Route path="/test-pogs" element={<NewPage />} />
